b06a22a55831009b71110f74fb2580fbd616772f
Mary Lechuga <marylechugaromero@gmail.com>
2026-05-05 23:50:22 -0600

    fix smtp port 587 y cambios index

--- server/server.js
+++ server/server.js
@@ -29,11 +29,10 @@ const ContactoSchema = new mongoose.Schema({
 });
 const Contacto = mongoose.model('Contacto', ContactoSchema);
 
-// ── Configurar correo (Gmail) ──
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
